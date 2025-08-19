7a708242d4de9df3d7f6dac504f2d5af27a56296
Dreamless07 <saadkhan7903@gmail.com>
2025-08-19 11:22:26 +0530

    practicing objects

--- src/objects.js
+++ src/objects.js
@@ -13,9 +13,24 @@ let data = {
         }
     }
 };
-
+console.log(data)
 console.log(data.name.firstname);
 console.log(data.name.lastname);
 console.log(data.branch.CSE.C1);
 console.log(data.branch.CSE.C2);
 console.log(data.branch.Mech.M1);
+
+for (key in data.branch){
+    console.log(key)// to get only key values from objects
+}
+
+//functions and objects
+let func={
+    "name":"ajay",
+    "age":"22",
+     "add":function()
+    {
+    console.log(func.name)//can use "this" keyword to fetch all objects of "name"
+    }
+}
+func.add()//object name.function name to call it
